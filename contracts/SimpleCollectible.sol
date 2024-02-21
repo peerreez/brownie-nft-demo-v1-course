9cc49107ca1e62078d1ec003465d1050977f6468
peerreez <dperezsx@gmail.com>
2024-02-21 12:36:20 +0100

    first commit

---
+++ contracts/SimpleCollectible.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.6;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract SimpleCollectible is ERC721 {
+    constructor() public ERC721("Dogie", "DOG") {}
+}
