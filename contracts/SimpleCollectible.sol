42c7fcd2837fbb44dbceece9960278319964e4fa
peerreez <dperezsx@gmail.com>
2024-02-24 14:09:42 +0100

    error value

--- contracts/SimpleCollectible.sol
+++ contracts/SimpleCollectible.sol
@@ -1,15 +1,16 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.6.6;
+pragma solidity 0.6.6;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract SimpleCollectible is ERC721 {
     uint256 public tokenCounter;
+    
+    constructor () public ERC721 ("Dogie", "DOG"){
+        tokenCounter = 0;
+    }
 
-    constructor() public ERC721("Dogie", "DOG") {}
-    function createCollectible(
-        string memory tokenURI
-    ) public returns (uint256) {
+    function createCollectible(string memory tokenURI) public returns (uint256){
         uint256 newTokenId = tokenCounter;
         _safeMint(msg.sender, newTokenId);
         _setTokenURI(newTokenId, tokenURI);
--- tests/unit/test_simple_collectible.py
+++ tests/unit/test_simple_collectible.py
@@ -1,4 +1,4 @@
-from scripts.deploy_and_create import deploy_and_create
+from scripts.simple_collectible.deploy_and_create import deploy_and_create
 from scripts.helpful_scripts import get_account, LOCAL_BLOCKCHAIN_ENVIRONMENTS
 from brownie import network
 import pytest
